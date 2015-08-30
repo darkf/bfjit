01707f66d48e07dd1758b1f1b87a9d939782ab5b
darkf <lw9k123@gmail.com>
2015-08-30 07:58:23 -0700

    implement looping

--- bfjit.cpp
+++ bfjit.cpp
@@ -1,43 +1,69 @@
 #include <cstdio>
 #include <asmjit/asmjit.h>
 #include <vector>
+#include <stack>
 using namespace asmjit;
 using namespace asmjit::x86;
 
 int main(int argc, char *argv[]) {
-	std::vector<char> cells(256); // construct 256 cells
+	std::vector<char> cells(1024); // construct 256 cells
+	std::stack<std::pair<Label, Label>> block_labels;
 	JitRuntime runtime;
 	X86Compiler c(&runtime);
 
-	cells.assign(256, 0);
+	cells.assign(1024, 0);
 
-	const char code[] = "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.";
+	const char code[] = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
 
 	c.addFunc(kFuncConvHost, FuncBuilder1<void, char*>());
 
 	X86GpVar cells_ptr(c, kVarTypeIntPtr, "cells_ptr");
 	c.setArg(0, cells_ptr);
-	X86GpVar a(c, kVarTypeInt32, "a");
-	//X86GpVar b(c, kVarTypeInt32, "b");
+	X86GpVar a(c, kVarTypeInt8, "a");
+	X86GpVar b(c, kVarTypeInt32, "b");
 
 	X86GpVar _putchar(c, kVarTypeIntPtr, "_putchar");
 	c.mov(_putchar, intptr_t(&putchar));
+
+	//Label lbl(c);
+	//Label lbl2(c);
 
 	for(char ch : code) {
 		switch(ch) {
 			case '+':
 				c.inc(byte_ptr(cells_ptr));
 				break;
+			case '-':
+				c.dec(byte_ptr(cells_ptr));
+				break;
 			case '<':
-				c.sub(cells_ptr, 4);
+				c.dec(cells_ptr);
 				break;
 			case '>':
-				c.add(cells_ptr, 4);
+				c.inc(cells_ptr);
 				break;
 			case '.': {
-				c.mov(a, byte_ptr(cells_ptr));
+				c.mov(b, byte_ptr(cells_ptr));
 				X86CallNode *call = c.call(_putchar, kX86FuncConvCDecl, FuncBuilder1<int, int>());
-				call->setArg(0, a);
+				call->setArg(0, b);
+				break;
+			}
+			case '[': {
+				// a pair of addresses for (start of loop, end of loop)
+				block_labels.emplace(std::make_pair(Label(c), Label(c)));
+				auto& label_pair = block_labels.top();
+
+				c.bind(label_pair.first);
+				c.mov(a, byte_ptr(cells_ptr));
+				c.test(a, a);
+				c.jz(label_pair.second);
+				break;
+			}
+			case ']': {
+				auto& label_pair = block_labels.top();
+				block_labels.pop();
+				c.jmp(label_pair.first);
+				c.bind(label_pair.second); // back-patch the label to refer to here (after the ])
 				break;
 			}
 			case '\0':
